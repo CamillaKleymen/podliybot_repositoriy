280ff6653c189e0e60443ef416308d5792dcb889
Camilla Kleymen <159798451+CamillaKleymen@users.noreply.github.com>
2024-02-25 17:50:50 +0500

    Add files via upload

--- podliybot.py
+++ podliybot.py
@@ -1,31 +1,39 @@
 import telebot
-import podliybuttons as pb 
+import podliybuttons as pb
 import podliydb as pd
-from geopy import Nominatim 
+from geopy import Nominatim
+import webbrowser
 
 bot = telebot.TeleBot('7094307129:AAHf8MgTkC9_u-7EtYUnGyngDNT4-IJyLfs')
-geolocator = Nominatim(user_agent= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36')
+geolocator = Nominatim(
+    user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0'
+    'Safari/537.36'
+)
+
 
 @bot.message_handler(commands=['start'])
-def start (message):
-    user_id = message.from_user.id
-    check = pd.check_user(user_id)
+
+def start(message):
+    gr_id = message.from_user.id
+    check = pd.check_user(gr_id)
     if check:
-        bot.send_message(user_id, "Hello, I'm glad to see you)",
-                         reply_markup = telebot.types.ReplyKeyboardRemove())
+        bot.send_message(gr_id, "Hello, I'm glad to see you",
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
     else:
-        bot.send_message(user_id, 'Hii!'
-                                  'Lets do registration!\n'
+        bot.send_message(gr_id, 'Hi!\n'
+                                  'Let\'s do registration!\n'
                                   'Enter your name',
-                                reply_markup=telebot.types.ReplyKeyboardRemove() ) 
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
         bot.register_next_step_handler(message, get_name)
 
+
 def get_name(message):
-    user_id=message.from_user.id
+    user_id = message.from_user.id
     user_name = message.text
     bot.send_message(user_id, 'Cool, and now send me your number please!',
-                     reply_markup = pb.user_num())
+                     reply_markup=pb.user_num())
     bot.register_next_step_handler(message, get_number, user_name)
+
 
 def get_number(message, user_name):
     user_id = message.from_user.id
@@ -36,21 +44,49 @@ def get_number(message, user_name):
         bot.register_next_step_handler(message, get_location,
                                        user_name, user_number)
     else:
-        bot.send_message(user_id, 'Send a number through button!!',
-                         reply_markup = pb.user_num())
+        bot.send_message(user_id, 'Send a number through the button!!',
+                         reply_markup=pb.user_num())
         bot.register_next_step_handler(message, get_number, user_name)
 
+
 def get_location(message, user_name, user_number):
-    user_id=message.from_user.id
+    user_id = message.from_user.id
     if message.location:
-        user_location = geolocator.reverse(f'{message.location.longitude}, '
-                                           f'{message.location.latitude}')
-        pd.register(user_id, user_name, user_number, user_location)
-        bot.send_message((user_id, 'Registration passed successfully'))
+        user_location = geolocator.reverse(f'{message.location.latitude}, '
+                                           f'{message.location.longitude}')
+        pd.register(user_id, user_name, user_number, str(user_location))
+        bot.send_message(user_id, 'Registration passed successfully')
     else:
-        bot.send_message(user_id, 'Send a location through button!!',
-                          reply_markup=pb.user_loc()) 
+        bot.send_message(user_id, 'Send a location through the button!!',
+                         reply_markup=pb.user_loc())
         bot.register_next_step_handler(message, get_location,
                                        user_name, user_number)
-         
+
+
+@bot.message_handler(commands= ['site'])
+def site(message):
+    webbrowser.open('https://github.com/CamillaKleymen/podliybot_repositoriy.git?authuser=0')
+@bot.message_handler(commands=['hello'])
+def main(message):
+    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}{message.from_user.last_name}')
+
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, 'you will not get a kind a help')
+
+
+@bot.message_handler(commands=['fishkibota'])
+def main(message):
+    bot.send_message(message.chat.id, 'This bot was created in a deep night, when a creator did not know which bot create. So and then author decided create a bot which can find different person and make potential friends ')
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'hello':
+        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}{message.from_user.last_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+    elif message.text.lower() == '/start':
+        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}{message.from_user.last_name}')
+
 bot.polling()
--- podliydb.py
+++ podliydb.py
@@ -7,23 +7,77 @@ sql.execute('CREATE TABLE IF NOT EXISTS podlie_users ('
             'id INTEGER,'
             'name TEXT, '
             'number TEXT, '
-            'location TEXT'
+            'location TEXT,'
+            'datereg REAL'
             ');')
 
-sql.execute('CREATE TABLE IF NOT EXISTS usergroup ('
-            'id INTEGER, '
-            'user_pr_name TEXT, '
-            'user_pr_count INTEGER'
+sql.execute('CREATE TABLE IF NOT EXISTS usergroups ('
+            'gr_id INTEGER, '
+            'usergr_name TEXT, '
+            'users_count INTEGER'
+            'cr_date REAL'
             ');')
 
-def check_user(id):
-    check = sql.execute('SELECT * FROM podlie_users WHERE id=?;', (id,))
+
+def check_user(gr_id):
+    check = sql.execute('SELECT * FROM podlie_users WHERE id=?;', (gr_id,))
     if check.fetchone():
         return True
     else:
         return False
 
+
 def register(id, name, number, location):
-    sql.execute('INSERT INTO podliye_users VALUES(?, ?, ?, ?);', (id, name, number, location))
+    sql.execute('INSERT INTO podlie_users VALUES(?, ?, ?, ?);', (id, name, number, location))
 
     connection.commit()
+
+def get_user():
+    return sql.execute('SELECT id, usergr_name, users_count, cr_date FROM podlie_users;').fetchall()
+
+def get_ex_gr(gr_id):
+    return sql.execute('SELECT gr_id, usergr_name, users_count' 
+                       'FROM usergroups where gr_id =?;', (gr_id,)).fetchone()
+
+def add_us_ingroup(user_id, user_name, user_gr):
+    sql.execute(' INSERT INTO podlie_users VALUES(?, ?, ?);', (user_id, user_name, user_gr))
+    connection.commit()
+
+def add_us(gr_id, usergr_name, users_count, cr_date):
+    sql.execute('INSERT INTO usergroups(gr_id, usergr_name, users_count, cr_date)'
+                'VALUES(?, ?, ?, ?);', (gr_id, usergr_name, users_count, cr_date))
+    connection.commit()
+
+def del_gr(gr_id):
+    sql.execute('DELETE FROM usergroups WHERE gr_id = ?;', (gr_id,))
+    connection.commit()
+
+def check_gr():
+    gr_check = sql.execute('SELECT * FROM usergroups;')
+    if gr_check.fetchone():
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
